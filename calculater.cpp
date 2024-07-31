9036b7325911cdef86b3217d3ccf25ae9ce6821b
jatin gurjar <129748129+jatin-gurjar-enjoy@users.noreply.github.com>
2024-07-31 21:44:54 +0530

    Add files via upload

---
+++ calculater.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float x,y;
+    cout<<"Enter the number 1 "endl;
+    cin>>x;
+    cout<<"Enter the number 2 "endl;
+    cin>>y;
+    cout<<"  Addition is:  "<<x+y;
+    cout<<"  Sbtraction  is: "<<x-y;
+    cout<<"  Multiplication is:  "<<x*y;
+    cout<<"  Division is:  "<<x/y;
+} 
